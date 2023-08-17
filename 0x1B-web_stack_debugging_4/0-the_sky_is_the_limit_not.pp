b3bd1dea33ce35a9f3959aa36866fa57a6e5a556
Kehinde Omokungbe <Kehindefranklin@gmail.com>
2023-08-17 10:01:26 -0700

    Fixing nginx limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
-# fix limit file at hbton user.
+# fix request limit at nginx
 
-exec { 'fix_limit_hbton_user':
-  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit hbton ser.
-exec { 'increase_soft_file':
-  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }

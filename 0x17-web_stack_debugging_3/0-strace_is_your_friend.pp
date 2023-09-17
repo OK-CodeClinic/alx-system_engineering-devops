df442d758a8d5fb5e763134650d9ddf4c6450646
Kehinde Omokungbe <Kehindefranklin@gmail.com>
2023-09-17 13:03:55 -0700

    Updating strace files

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes apache issue conf file
+# Fix 500 error when a GET HTTP method is requested to Apache web server
 
-exec { 'fix-apache-error':
-  command     => '/opt/apache/fix_script.sh',
-  refreshonly => true,
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
